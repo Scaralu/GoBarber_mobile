035096ca6286c77d4e7909b17166dda0da2d71ad
Lucca Scarano <lucca.scarano@hotmail.com>
2020-07-31 23:46:37 -0300

    font assets and login page

---
+++ src/components/Button/styles.ts
@@ -0,0 +1,21 @@
+import styled from 'styled-components/native';
+import { RectButton } from 'react-native-gesture-handler';
+
+
+export const Container = styled(RectButton)`
+    width: 90%;
+    height: 60px;
+    background: #FF9000;
+    border-radius: 10px;
+    padding: 0 30px;
+
+    margin-top: 10px;
+    justify-content: center;
+    align-items: center;
+`;
+
+export const ButtonText = styled.Text`
+    font-family: 'RobotoSlab-Medium';
+    color: #312E38;
+    font-size: 18px;
+`;
